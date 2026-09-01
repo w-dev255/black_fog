8105509a3df60dc86985b87950c4b05b70ac81d6
w-dev255 <devangleteam@gmail.com>
2026-09-01 14:05:09 +0200

    update

--- src/main.c
+++ src/main.c
@@ -1,37 +1,44 @@
 #include "lib/IO/IO.h"
-#include "lib/game/game.h"
+/*#include "lib/game/game.h"*/
 
 int main(int argc, char* argv[]) {
 	if (argc <= 1) {
 		return 1;
 	}
 
-	const char* begining = "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e";
+	long int begining = 185403245329964;
 
 	if (strcmp(argv[1], "--hacker") == 0) {
-		ONELINE oneline = {0};
+		ONELINE oneline = calloc(1, sizeof(ONELINE));
+		if (oneline == NULL) {
+			return 1;
+		}
 
 		while (1) {
 			fflush(stdout);
 
 			printf("\n\n|-------------------------------------------|\n|                   MENU                    |\n|-------------------------------------------|\n\n");
 
-			printf("	0. quit\n\n");
+			printf("	0. Start\n	1. Load\n	2.Quit\n\n");
 
 			printf("user: ");
 			oneline_input(&oneline);
 
 			if (strlen(oneline.buffer) == 0) {
 				continue;
-			} else {
-				if (strcmp(oneline.buffer, "0") == 0 || strcmp(oneline.buffer, "quit") == 0) {
-					destroy_buffer(&oneline);
-
-					return 0;
+			} else if (strcmp(oneline.buffer, "0") == 0 || strcmp(oneline.buffer, "Start") == 0) {
+					printf("Placeholder\n");
 				}
+			} else if (strcmp(oneline.buffer, "1") == 0 || strcmp(oneline.buffer, "Load") == 0) {
+				printf("Placeholder\n");
+			} else if (strcmp(oneline.buffer, "2") == 0 || strcmp(oneline.buffer, "Quit") == 0) {
+				destroy_buffer(&oneline);
+
+				destroy_oneline(&oneline);
+
+				return 0;
 			}
 		}
-		destroy_buffer(&oneline);
 	}
 
 	return 0;
